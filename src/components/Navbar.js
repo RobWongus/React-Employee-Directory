f88863ebbe3cf3e3dbc812a2e9dcd6f35982227b
Robert Wongus <rob_wo@yahoo.com>
2020-06-04 18:12:51 -0400

    added search button and jumbotrom from react bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
 
 export default function Navbar() {
     return (
         <div>
-            T'Sup
+           <Jumbotron fluid>
+            <Container>
+                <h1>Fluid jumbotron</h1>
+                 <p>Employee Directory</p>
+            </Container>
+           </Jumbotron>
         </div>
     )
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
 export default function Search() {
     return (
         <div>
-            HI!
+           <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+           </Form>
         </div>
 
     )
