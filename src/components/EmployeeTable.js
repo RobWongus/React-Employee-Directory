cfc942c82aad5df23899a8d0a78cf9f63d1966e0
Robert Wongus <rob_wo@yahoo.com>
2020-06-12 19:51:34 -0400

    changed SSH key to git HTTP for homepage in packahe json

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -33,3 +33,28 @@ const EmployeeTable = (props) => {
 }
 export default EmployeeTable
 
+
+{/* <tr> 
+<td>Mark Bunch</td>
+<td>mark@markymark.com</td>
+<td>617-922-8810</td>
+<td>07/26/1968</td>
+</tr>
+<tr>
+<td>James Howlett</td>
+<td>weaponx@alphaflight.com</td>
+<td>416-681-3719</td>
+<td>10/01/1880</td>
+</tr>
+<tr>
+<td>Nathan Summers</td>
+<td>bodyslidetohq@xforce.com</td>
+<td>813-735-9173</td>
+<td>01/18/2045</td>
+</tr>
+<tr>
+<td>Piotr Rasputin</td>
+<td>insovietrussia@yakov.com</td>
+<td>7-940-318-65-48</td>
+<td>05/01/1975</td>
+</tr> */}
--- src/utils/sort.js
+++ src/utils/sort.js
@@ -40,12 +40,3 @@ return sortEmployees
 
 module.exports= {sortName}
 
-// import axios from "axios";
-
-// const BASEURL = "https://randomuser.me/api/?results=10&nat=us";
-
-// export default {
-//     search: function(){
-//         return axios.get(BASEURL);
-//     }
-// };
